c837a020aca587e6cfce7d0075440a4a6a64a087
Angel Guevara <aguevara@tecnocen.com>
2016-04-24 20:13:14 -0500

    simplificar autoloader sin composer

--- quiubas.php
+++ quiubas.php
@@ -9,15 +9,16 @@ $required_extensions = array(
 
 foreach ($required_extensions as $v) {
     if (extension_loaded($v) === false) {
-        include __DIR__ . 'quiubas/Exception.php';
-        throw new \Quiubas\Exception('The "' . $v . '" extension is required by Quiubas Library');
+        include __DIR__ . '/Quiubas/Exception.php';
+        throw new \Quiubas\Exception(
+            'The "' . $v . '" extension is required by Quiubas Library'
+        );
         break;
     }
 }
 
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-    if (file_exists($file) === true) {
-        require_once $file;
-    }
+    $file = __DIR__ . DIRECTORY_SEPARATOR .
+        str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    include $file;
 }, false, true);
