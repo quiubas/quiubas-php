6426c4af59086683205dd9d14d15ed9fcf18168c
Angel Guevara <aguevara@tecnocen.com>
2016-04-24 20:08:52 -0500

    Soporte psr4

--- quiubas.php
+++ quiubas.php
@@ -2,13 +2,22 @@
 
 namespace Quiubas;
 
-if ( defined( 'QUIUBAS_BASE_PATH' ) === false ) {
-	define( 'QUIUBAS_BASE_PATH', dirname( __FILE__ ) . '/' );
+$required_extensions = array(
+    'openssl',
+    'curl'
+);
+
+foreach ($required_extensions as $v) {
+    if (extension_loaded($v) === false) {
+        include __DIR__ . 'quiubas/Exception.php';
+        throw new \Quiubas\Exception('The "' . $v . '" extension is required by Quiubas Library');
+        break;
+    }
 }
 
-spl_autoload_register(function($class) {
-	$class = QUIUBAS_BASE_PATH . strtolower(str_replace('\\', '/', $class)) . '.php';
-	if ( file_exists($class) === true ) {
-		require_once $class;
-	}
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    if (file_exists($file) === true) {
+        require_once $file;
+    }
 }, false, true);
